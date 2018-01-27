a64947eb0c37fbf6addb42dcb335c6087b3b4408
Tarang Parikh <tp0265@gmail.com>
2018-01-27 06:07:53 +0530

    Auto stash before merge of "master" and "origin/master"

---
+++ ps-api-gateway/src/main/java/com/pharmaerp/psapigateway/PsApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.pharmaerp.psapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsApiGatewayApplication.class, args);
+	}
+}
